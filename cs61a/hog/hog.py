7870b81100ec8e60ae5acfad991d7b6148d182ff
elderhammer <81279860+elderhammer@users.noreply.github.com>
2024-02-08 21:29:48 +0800

    finish hog problem 6

--- cs61a/hog/hog.py
+++ cs61a/hog/hog.py
@@ -162,6 +162,7 @@ def play(strategy0, strategy1, score0=0, score1=0, dice=six_sided,
             if is_swap(score1, score0):
                 score0, score1 = score1, score0
         who = other(who)
+        say = say(score0, score1)
     # END PROBLEM 5
     # (note that the indentation for the problem 6 prompt (***YOUR CODE HERE***) might be misleading)
     # BEGIN PROBLEM 6
